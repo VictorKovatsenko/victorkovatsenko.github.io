d659cf98c149f24ea6f903eacd7c03f6e26f56ab
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-02 21:36:59 +0200

    Language Selector Script fixed

--- language_options.js
+++ language_options.js
@@ -1,6 +1,6 @@
 const languageSelector = document.getElementById("language-selector");
 languageSelector.addEventListener("change", function() {
-  const selectedValue = this.value;
+  const selectedValue = this.value.toLowerCase(); // convert to lowercase
   if (selectedValue === "en") {
     window.location.href = "index.html";
   } else {
