4d08d0672d310525d3f3ed23ece44a6f53c8bc4e
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-03 18:19:50 +0200

    JS instagram redirect updated

--- instagram_redirect.js
+++ instagram_redirect.js
@@ -1,11 +1,11 @@
-var isInstagramApp = (window.navigator.userAgent.includes('Instagram') || window.navigator.userAgent.includes('IGTV'));
+var isInstagramApp = (window.navigator.userAgent.includes('instagram') || window.navigator.userAgent.includes('IGTV'));
 
 if (isInstagramApp && /(android)/i.test(navigator.userAgent)) {
   // Instagram app on Android
-  document.getElementById("redirect-link-2").href = "intent://app/@berlin_lashes_brows/#Intent;package=com.instagram.android;scheme=https;end";
+  document.getElementById("redirect-link-2").href = "https://instagram.com/berlin_lashes_brows/";
 } else if (isInstagramApp && /(iphone|ipad)/i.test(navigator.userAgent)) {
   // Instagram app on iOS
-  document.getElementById("redirect-link-2").href = "instagram://user?username=berlin_lashes_brows";
+  document.getElementById("redirect-link-2").href = "https://instagram.com/berlin_lashes_brows/";
 } else {
   // Not browsing from Instagram app
   document.getElementById("redirect-link-2").href = "https://ig.me/m/berlin_lashes_brows/";
