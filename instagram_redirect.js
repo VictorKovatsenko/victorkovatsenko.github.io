3932ec380eeab1f7c0c8715c81a44737ba489a59
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-03 20:59:57 +0200

    Redirect switched on

--- instagram_redirect.js
+++ instagram_redirect.js
@@ -1,7 +1,7 @@
-// if (navigator.userAgent.match(/(instagram)/i))  {
-//     document.getElementById("redirect-link-2").href = "https://instagram.com/berlin_lashes_brows/";
-//     document.getElementById("redirect-link-1").href = "https://instagram.com/berlin_lashes_brows/";
-// }  else {
-//     document.getElementById("redirect-link-2").href = "https://ig.me/m/berlin_lashes_brows/";
-//     document.getElementById("redirect-link-1").href = "https://ig.me/m/berlin_lashes_brows/";
-// }
+if (navigator.userAgent.match(/(instagram)/i))  {
+    document.getElementById("redirect-link-2").href = "https://instagram.com/berlin_lashes_brows/";
+    document.getElementById("redirect-link-1").href = "https://instagram.com/berlin_lashes_brows/";
+}  else {
+    document.getElementById("redirect-link-2").href = "https://ig.me/m/berlin_lashes_brows/";
+    document.getElementById("redirect-link-1").href = "https://ig.me/m/berlin_lashes_brows/";
+}
