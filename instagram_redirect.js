50c95a5ea88b542bfd8bdb716384ffe7eedd757a
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-03 17:56:10 +0200

    JS inst redirect updated

--- instagram_redirect.js
+++ instagram_redirect.js
@@ -1,5 +1,5 @@
-  if (window.location.href.includes("instagram.com")) {
-    document.getElementById("book-now").href = "https://www.instagram.com/berlin_lashes_brows/?hl=en&tab=messages";
-  } else {
+if (window.location.href.includes("instagram.com")) {
+    document.getElementById("book-now").href = "https://instagram.com/berlin_lashes_brows/";
+} else {
     document.getElementById("book-now").href = "https://ig.me/m/berlin_lashes_brows/";
-  }
+}
