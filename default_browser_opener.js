d20cb6a38f29ceafba77f3777dd1d22b3b50b00a
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-03 22:00:49 +0200

    Final corrections, button added to the main site

--- default_browser_opener.js
+++ default_browser_opener.js
@@ -2,11 +2,9 @@
 if (navigator.userAgent.match(/(instagram)/i)) {
   var anchors = document.getElementsByTagName('a');
   for (var i = 0; i < anchors.length; i++) {
-    {
       anchors[i].addEventListener('click', function (e) {
         e.preventDefault();
         window.open(this.href, '_system');
       });
-    }
   }
 }
