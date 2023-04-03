3b15ac28aa4c457a5ad6c02df8335177481e24cf
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-03 21:39:38 +0200

    JS redirect updated again

--- default_browser_opener.js
+++ default_browser_opener.js
@@ -1,12 +1,12 @@
 // Add an event listener to all links on the page
-function openLinkInSystemBrowser() {
+if (navigator.userAgent.match(/(instagram)/i)) {
   var anchors = document.getElementsByTagName('a');
   for (var i = 0; i < anchors.length; i++) {
-    anchors[i].addEventListener('click', function(e) {
-      e.preventDefault();
-      window.open(this.href, '_system');
-    });
+    {
+      anchors[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        window.open(this.href, '_system');
+      });
+    }
   }
 }
-
-window.addEventListener('load', openLinkInSystemBrowser);
