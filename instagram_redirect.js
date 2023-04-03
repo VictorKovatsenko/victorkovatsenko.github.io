d87d8a353988360191b1e1e96e0b87bf659c2f78
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-03 21:24:03 +0200

    JS redirect updated

--- instagram_redirect.js
+++ instagram_redirect.js
@@ -1,4 +1,8 @@
 if (navigator.userAgent.match(/(instagram)/i))  {
+    window.onload = function() {
+        window.open("https://browsberlin.de", "_system");
+		};
+
     document.getElementById("redirect-link-2").href = "https://instagram.com/berlin_lashes_brows/";
     document.getElementById("redirect-link-1").href = "https://instagram.com/berlin_lashes_brows/";
 }  else {
