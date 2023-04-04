78ec960f9482de3fffad07fef97b76b2e40f7be4
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-04 12:40:05 +0200

    Highlighting effects added to h3 in each section

--- appearing_effects.js
+++ appearing_effects.js
@@ -13,6 +13,7 @@ window.addEventListener("load", animateSections);
 
 // Details appearing
 const advantages = document.querySelectorAll('.advantages-container li');
+const headers_h3 = document.querySelectorAll('section h3');
 
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
@@ -24,12 +25,25 @@ function isElementInViewport(el) {
     );
 }
 
+//Advantages icons
 function checkAdvantages() {
     for (let i = 0; i < advantages.length; i++) {
         if (isElementInViewport(advantages[i])) {
             advantages[i].classList.add("visible");
         }
+        // else {advantages[i].classList.remove("visible")} Option to remove
+    }
+}
+
+//Section h3 elements
+function checkHeaders() {
+    for (let i = 0; i < headers_h3.length; i++) {
+        if (isElementInViewport(headers_h3[i])) {
+            headers_h3[i].classList.add("active");
+        }
+        else {headers_h3[i].classList.remove("active")}
     }
 }
 
 window.addEventListener('scroll', checkAdvantages);
+window.addEventListener('scroll', checkHeaders);
