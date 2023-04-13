c36cba750783271e56ab7d9d4fac82d04e22e69a
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-13 11:43:37 +0200

    Code reformatted

--- scripts/language_options.js
+++ scripts/language_options.js
@@ -1,17 +1,17 @@
 const languageSelector = document.getElementById("language-selector");
-languageSelector.addEventListener("change", function() {
-  const selectedValue = this.value.toLowerCase(); // convert to lowercase
-  const scriptElement = document.querySelector('script[src="language_redirect.js"]'); // script disabling
+languageSelector.addEventListener("change", function () {
+    const selectedValue = this.value.toLowerCase(); // convert to lowercase
+    const scriptElement = document.querySelector('script[src="language_redirect.js"]'); // script disabling
 
-  // Redirect to selected value
-  if (selectedValue === "en") {
-    window.location.href = "index.html";
-    // Disable automatic redirection on page loading
-    if (scriptElement) {
-    // Remove the script element from the DOM
-    scriptElement.parentNode.removeChild(scriptElement);
+    // Redirect to selected value
+    if (selectedValue === "en") {
+        window.location.href = "index.html";
+        // Disable automatic redirection on page loading
+        if (scriptElement) {
+            // Remove the script element from the DOM
+            scriptElement.parentNode.removeChild(scriptElement);
+        }
+    } else {
+        window.location.href = "index_" + selectedValue + ".html";
     }
-  } else {
-    window.location.href = "index_" + selectedValue + ".html";
-  }
 });
