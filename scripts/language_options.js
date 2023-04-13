f5e6c4b7c944be2e11cbd1c875bad38d704278b8
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-13 12:03:10 +0200

    Language redirect experiments

--- scripts/language_options.js
+++ scripts/language_options.js
@@ -1,16 +1,21 @@
 const languageSelector = document.getElementById("language-selector");
+
+languageSelector.addEventListener("load", function () {
+    const userLang = navigator.language;
+    if (userLang.toLowerCase().startsWith('de')) {
+      window.location.href = 'index_de.html'; // Redirect to German version
+    } else if (userLang.toLowerCase().startsWith('ru')) {
+      window.location.href = 'index_ru.html'; // Redirect to Russian version
+    } else {
+      window.location.href = 'index.html'; // Redirect to English version or default language
+    }
+});
+
 languageSelector.addEventListener("change", function () {
     const selectedValue = this.value.toLowerCase(); // convert to lowercase
-    const scriptElement = document.querySelector('script[src="language_redirect.js"]'); // script disabling
-
     // Redirect to selected value
     if (selectedValue === "en") {
         window.location.href = "index.html";
-        // Disable automatic redirection on page loading
-        if (scriptElement) {
-            // Remove the script element from the DOM
-            scriptElement.remove();
-        }
     } else {
         window.location.href = "index_" + selectedValue + ".html";
     }
