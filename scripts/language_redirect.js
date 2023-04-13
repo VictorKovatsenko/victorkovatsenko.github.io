ad1da88221f0ba23309d0ca2d1a07c2e70c3acf6
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-13 11:05:40 +0200

    Language redirection logic

---
+++ scripts/language_redirect.js
@@ -0,0 +1,9 @@
+// Language redirection logic
+var userLang = navigator.language;
+if (userLang.toLowerCase().startsWith('de')) {
+  window.location.href = 'index_de.html'; // Redirect to German version
+} else if (userLang.toLowerCase().startsWith('ru')) {
+  window.location.href = 'index_ru.html'; // Redirect to Russian version
+} else {
+  window.location.href = 'index.html'; // Redirect to English version or default language
+}
