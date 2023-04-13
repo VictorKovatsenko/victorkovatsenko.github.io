ce3de0ee2575b20ad807bc5b066ba1077c3780cb
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-13 11:42:59 +0200

    Language redirection fixes continue

--- scripts/language_options.js
+++ scripts/language_options.js
@@ -1,17 +1,16 @@
 const languageSelector = document.getElementById("language-selector");
 languageSelector.addEventListener("change", function() {
   const selectedValue = this.value.toLowerCase(); // convert to lowercase
-  const scriptElement = document.querySelector('script[src="language_redirection.js"]'); // script disabling
-
-  // Disable automatic redirection on page loading
-  if (scriptElement) {
-    // Remove the script element from the DOM
-    scriptElement.parentNode.removeChild(scriptElement);
-  }
+  const scriptElement = document.querySelector('script[src="language_redirect.js"]'); // script disabling
 
   // Redirect to selected value
   if (selectedValue === "en") {
     window.location.href = "index.html";
+    // Disable automatic redirection on page loading
+    if (scriptElement) {
+    // Remove the script element from the DOM
+    scriptElement.parentNode.removeChild(scriptElement);
+    }
   } else {
     window.location.href = "index_" + selectedValue + ".html";
   }
