7d8775478a7b34a7c56e726723094a3598b74b35
Victor Kovatsenko <victor.kovatsenko@klarna.com>
2023-04-13 12:27:32 +0200

    Language redirection turned off

--- scripts/language_options.js
+++ scripts/language_options.js
@@ -1,16 +1,5 @@
 const languageSelector = document.getElementById("language-selector");
 
-window.addEventListener("load", function () {
-    const userLang = navigator.language;
-    if (userLang.toLowerCase().startsWith('de')) {
-      window.location.href = 'index_de.html'; // Redirect to German version
-    } else if (userLang.toLowerCase().startsWith('ru')) {
-      window.location.href = 'index_ru.html'; // Redirect to Russian version
-    } else {
-      window.location.href = 'index.html'; // Redirect to English version or default language
-    }
-});
-
 languageSelector.addEventListener("change", function () {
     const selectedValue = this.value.toLowerCase(); // convert to lowercase
     // Redirect to selected value
